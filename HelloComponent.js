7999d231e715b08e3122df2744e999ef8c80d741
lyx0206331 <rq880113@163.com>
2018-03-15 18:07:31 +0800

    Initial commit

---
+++ HelloComponent.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+/**
+ * 方式一:ES6
+ * 推荐
+ */
+// export default class HelloComponent extends Component {
+//     render() {
+//         return <Text style={{fontSize:20,backgroundColor:'red'}}>Hello</Text>
+//     }
+// }
+
+/**
+ * 方式二：ES5
+ */
+// var HelloComponent=React.createClass({
+//     render(){
+//         return <Text style={{fontSize:20,backgroundColor:'red'}}>Hello</Text>
+//     }
+// })
+// module.exports=HelloComponent;
+
+/**
+ * 方式三：函数式
+ * 无状态，不能使用this
+ */
+function HelloComponent() {
+    return <Text style={{fontSize:20,backgroundColor:'red'}}>Hello</Text>
+}
+module.exports=HelloComponent;
---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+import setup from './setup'
+
+AppRegistry.registerComponent('imooc_simple', () => setup);
